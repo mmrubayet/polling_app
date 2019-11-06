bd173b0cbe5c58097000c68d3baa93a745497b02
Rubayet Mahmud <m.rubayet94@gmail.com>
2019-11-06 09:33:46 +0600

    Update models.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+import datetime
+from django.utils import timezone
 # Create your models here.
 
 class Question(models.Model):
@@ -12,6 +13,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        was_published_recently.admin_order_field = 'pub_date'
+        was_published_recently.boolean = True
+        was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
